157117b2fab3269119e2cd2c6a1d97d18bbfe938
swapsatoshi <swapsatoshi@gmail.com>
2024-07-23 00:27:09 +0800

    create folder for logo if not exists

--- services/gateway/shared/logo.js
+++ services/gateway/shared/logo.js
@@ -3,11 +3,13 @@ const path = require('path');
 
 const setLogo = async params => {
 	const basePath = path.join(__dirname, '../public/static/img', 'logo');
+	if (!fs.existsSync(basePath)) fs.mkdirSync(basePath);
 	fs.writeFileSync(path.join(basePath, params.fileName), Buffer.from(params.data, 'hex'));
 };
 
 const deleteLogo = async params => {
 	const basePath = path.join(__dirname, '../public/static/img', 'logo');
+	if (!fs.existsSync(basePath)) fs.mkdirSync(basePath);
 	fs.rmSync(path.join(basePath, params.fileName));
 };
 
